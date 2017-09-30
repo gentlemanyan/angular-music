e7ce9e11797b4efd82be962a7fbef666dcf8e3f8
gentleman <651193558@qq.com>
2017-10-01 00:33:53 +0800

    修改布局

--- src/app/home/recommend/recommend.component.ts
+++ src/app/home/recommend/recommend.component.ts
@@ -27,7 +27,30 @@ export class RecommendComponent implements OnInit {
       {src: 'assets/img/carousel_7.jpg', content: '测试，这是一个热门推荐'}
     ]
   ];
-  public localState = { value: '' };
+
+  public recomendArr = [
+    [
+      {src: 'assets/img/carousel_1.jpg', firstTitle: '每日歌曲推荐',
+      secondTile: '根据你的口味生成，每日更新', isPadding: false},
+      {src: 'assets/img/carousel_2.jpg', firstTitle: '周杰伦新专辑',
+       secondTile: '周杰伦的床边故事', isPadding: true},
+    ],
+    [
+      {
+        src: 'assets/img/carousel_3.jpg',
+        firstTitle: '陈奕迅新歌，快来买啊',
+        secondTile: '根据你的口味生成，每日更新',
+        isPadding: false
+      },
+      {
+        src: 'assets/img/carousel_4.jpg',
+        firstTitle: '瑶宝宝的新歌，求粉',
+        secondTile: '哈哈哈，瑶宝宝发新歌了',
+        isPadding: true
+      },
+    ]
+  ];
+
   public color = '#F00';
   /**
    * TypeScript public modifiers
